ad7c6a6fb6d5ab5595e930cd5694035d3b9aef29
JungMin Ahn <jungminahn.minsone@gmail.com>
2020-10-15 22:58:09 +0900

    modify

---
+++ 20201010_SampleXCFramework/sample/build/mac.xcarchive/Products/Library/Frameworks/sample.framework/Versions/A/Headers/sample.h
@@ -0,0 +1,18 @@
+//
+//  sample.h
+//  sample
+//
+//  Created by minsone on 2020/10/03.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sample.
+FOUNDATION_EXPORT double sampleVersionNumber;
+
+//! Project version string for sample.
+FOUNDATION_EXPORT const unsigned char sampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sample/PublicHeader.h>
+
+
